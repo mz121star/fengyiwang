a276655078477620cd6b333110b41317ded17188
ivan820819 <ivan820819@qq.com>
2014-08-31 16:31:20 +0800

    123

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -20,4 +20,80 @@ class IndexAction extends Action {
         $this->assign('sectionlist', $sectionlist);
         $this->display();
     }
+
+    public function reg() {
+        $this->display();
+    }
+
+    public function showlogin() {
+        $this->display();
+    }
+
+    public function getcode() {
+        $code=1243456;
+        //短信接口机构代码 $jgid
+        $jgid = '300';
+        //短信接口用户名 $loginname
+        $loginname = 'hummerlys';
+        //短信接口密码 $passwd
+        $passwd = '851514';
+        //发送到的目标手机号码 $telphone，多个号码用半角分号分隔
+        $telphone = $this->_post('phone');
+        //短信内容 $message
+        $message = "尊敬的客户，您的验证码是：$code【风翼网】";
+        $gateway = "http://IP:8180/service.asmx/SendMessageStr?Id=$jgid&Name=$loginname&Psw=$passwd&Message=$message&Phone=$telphone&Timestamp=0";
+        $result = file_get_contents($gateway);
+        echo $result;
+        exit;
+    }
+
+    public function login() {
+        $userInfo = session('userinfo');
+        if(!empty($userInfo) && $userInfo['user_type'] == 3 ){
+            $this->redirect('Index/index');
+        }
+        $user = M("User");
+        $_POST['user_id'] = $this->_post('user_id');
+        $_POST['user_pw'] = md5($this->_post('user_pw'));
+        $_POST['user_status'] = 1;
+        $userInfo = $user->where($_POST)->field('id,user_id,user_type')->find();
+        if(!empty($userInfo) && $userInfo['user_type'] == 3){
+            session('userinfo', $userInfo);
+        }
+        $this->redirect('Index/index');
+    }
+
+    public function logout() {
+        $userInfo = session('userinfo');
+        if(!empty($userInfo)){
+            session('userinfo', null);
+            unset($_SESSION['cart']);
+        }
+        $this->redirect('Index/index');
+    }
+    
+    public function doregist() {
+        $post = $this->filterAllParam('post');
+        if (!$post['user_id']) {
+            $this->error("用户名不能为空", 'index');
+        }
+        if (!$post['user_pw1']) {
+            $this->error("密码不能为空", 'index');
+        }
+        if ($post['user_pw1'] != $post['user_pw2']) {
+            $this->error("密码不一致", 'index');
+        }
+        $user = M("User");
+        $userInfo = $user->where('user_id="'.$post['user_id'].'"')->field('id')->find();
+        if ($userInfo) {
+            $this->error("用户ID已存在", 'index');
+        }
+        $post['user_pw'] = md5($post['user_pw1']);
+        $userid = $user->add($post);
+        if ($userid) {
+            $people = M("People");
+            $peopleid = $people->add(array('user_id'=>$post['user_id'], 'people_email'=>$post['people_email']));
+        }
+        $this->redirect('User/changeinfo');
+    }
 }
---
+++ Public/wx.php
@@ -0,0 +1,57 @@
+<?php
+
+define("TOKEN", "fengyiwang");
+$wxObj = new weixin();
+$wxObj->responseMsg();
+
+class weixin {
+
+    public function valid() {
+        $echoStr = $_GET["echostr"];
+        if ($this->checkSignature()) {
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    public function responseMsg() {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        if (!empty($postStr)){
+            $keyword = trim($postObj->Content);
+            mb_internal_encoding("UTF-8");
+        }
+
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $time = time();
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[您输入的功能未识别]]></Content>
+                    </xml>";
+        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
+        echo $resultStr;
+        exit;
+    }
+
+    private function checkSignature() {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
