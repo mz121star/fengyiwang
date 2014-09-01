f324257206210e086850529402e2c967976d40dd
zhaobin <zhaobin@deepin.deepin-domain>
2014-09-01 16:05:55 +0800

    add

--- Conf/config.php
+++ Conf/config.php
@@ -1,8 +1,7 @@
 <?php
 return array(
     'DB_PREFIX' => 'fy_',
-    'DB_DSN' => 'mysql://root:820819@localhost:3306/fengyi',
+    'DB_DSN' => 'mysql://root:8ecba89b81@localhost:3306/fengyi',
 
-    'SHOW_PAGE_TRACE' => true,
     'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes'
 );
