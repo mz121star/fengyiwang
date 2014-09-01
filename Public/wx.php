7db8887214c14bb39d34634f619145ccf6c30e80
zhaobin <zhaobin@deepin.deepin-domain>
2014-09-01 13:23:28 +0800

    add

--- Public/wx.php
+++ Public/wx.php
@@ -37,28 +37,42 @@ class weixin {
     }
     
     //接收事件消息
-    private function receiveEvent($object)
-        {
-        $content = "";
-        switch ($object->EventKey)
-        {
-        case "COMPANY":
-        $content = "聚优客为您提供互联网相关产品与服务。";
-        break;
-        default:
-        $content = "点击菜单：".$object->EventKey;
-        break;
+    private function receiveEvent($object)  {
+       $result = $this->transmitNews($object);
+        return $result;
+    }
+    
+    //回复图文消息
+    private function transmitNews($object) {
+        $itemTpl = " <item>
+        <Title><![CDATA[%s]]></Title>
+        <Description><![CDATA[%s]]></Description>
+        <PicUrl><![CDATA[%s]]></PicUrl>
+        <Url><![CDATA[%s]]></Url>
+        </item>
+        ";
+        if ($object->EventKey == 'fy_jblx') {
+            $item_str = sprintf($itemTpl, '结伴留学', '结伴留学', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName.'/jblx');
+        } elseif ($object->EventKey == 'fy_jbxx') {
+            $item_str = sprintf($itemTpl, '结伴学习', '结伴学习', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName.'/jbxx');
+        } elseif ($object->EventKey == 'fy_tglx') {
+            $item_str = sprintf($itemTpl, '团购留学', '团购留学', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName.'/tglx');
+        } elseif ($object->EventKey == 'fy_tgxx') {
+            $item_str = sprintf($itemTpl, '团购学习', '团购学习', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName.'/tgxx');
+        } elseif ($object->EventKey == 'fy_tjpy') {
+            $item_str = sprintf($itemTpl, '推荐朋友', '推荐朋友', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName.'/tjpy');
         }
         
-        if(is_array($content)){
-        if (isset($content[0]['PicUrl'])){
-        $result = $this->transmitNews($object, $content);
-        }else if (isset($content['MusicUrl'])){
-        $result = $this->transmitMusic($object, $content);
-        }
-        }else{
-        $result = $this->transmitText($object, $content);
-        }
+        $xmlTpl = "<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime>%s</CreateTime>
+        <MsgType><![CDATA[news]]></MsgType>
+        <ArticleCount>%s</ArticleCount>
+        <Articles>
+        $item_str</Articles>
+        </xml>";
+        $result = sprintf($xmlTpl, $object->FromUserName, $object->ToUserName, time(), 1);
         return $result;
     }
 
