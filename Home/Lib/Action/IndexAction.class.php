c39421bb9ec6746b1f70e26964318e5937efb687
zhaobin <zhaobin@deepin.deepin-domain>
2014-09-01 10:39:02 +0800

    add

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -96,4 +96,22 @@ class IndexAction extends Action {
         }
         $this->redirect('Index/index');
     }
+    
+    protected function filterAllParam($type = 'get') {
+        $param = array();
+        if ($type == 'get') {
+            foreach ($_GET as $key => $value) {
+                $param[$key] = $this->_get($key);
+            }
+        } elseif ($type == 'post') {
+            foreach ($_POST as $key => $value) {
+                $param[$key] = $this->_post($key);
+            }
+        } else {
+            foreach ($_REQUEST as $key => $value) {
+                $param[$key] = $this->_param($key);
+            }
+        }
+        return $param;
+    }
 }
