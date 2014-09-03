39352e0f8c00afb44af680d6a37e212bcaf284dc
ivan820819 <ivan820819@qq.com>
2014-09-03 21:11:46 +0800

    s

--- Conf/config.php
+++ Conf/config.php
@@ -3,5 +3,6 @@ return array(
     'DB_PREFIX' => 'fy_',
     'DB_DSN' => 'mysql://root:8ecba89b81@localhost:3306/fengyi',
 
-    'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes'
+    'DEFAULT_FILTER'=>'htmlspecialchars,stripslashes',
+    'ORDER_STATUS' => array('推荐确认', '推荐成功', '等待签约', '签约成功', '奖金发放', '发放成功')
 );
