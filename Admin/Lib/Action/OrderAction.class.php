10711c1ed11e1027cccc5b5c3724dd7e6491e799
zhaobin <zhaobin@deepin.deepin-domain>
2014-09-04 17:37:18 +0800

    add

--- Admin/Lib/Action/OrderAction.class.php
+++ Admin/Lib/Action/OrderAction.class.php
@@ -13,6 +13,18 @@ class OrderAction extends PublicAction {
         $this->assign('orderlist', $orderlist);
         $this->display();
     }
+    
+    public function jblists(){
+        $jborder = M("jborder");
+        import('ORG.Util.Page');
+        $count = $jborder->where('order_parent=0 and user_jbname != ""')->count();
+        $page = new Page($count, 10);
+        $orderlist = $jborder->field('fy_jborder.id, fy_jbedu.id as jid, user_jbname, user_jbphone, user_jbdesc, order_date, order_status, edu_name, edu_image')->where('order_parent=0 and user_jbname != ""')->join(' fy_jbedu on fy_jbedu.jborder_id=fy_jborder.id')->join(' fy_edu on fy_jbedu.edu_id=fy_edu.id')->limit($page->firstRow.','.$page->listRows)->select();
+        $show = $page->show();
+        $this->assign('page',$show);
+        $this->assign('orderlist', $orderlist);
+        $this->display();
+    }
 
     public function search(){
         $post = $this->filterAllParam('post');
@@ -41,22 +53,60 @@ class OrderAction extends PublicAction {
     }
 
     public function recommend(){
-        
+        $oid = $this->_get('oid');
+        $order = M("order");
+        $orderinfo = $order->where('id='.$oid)->find();
+        if ($orderinfo) {
+            $order->where('id='.$oid)->setField('order_status','1');
+        } else {
+            $jborder = M("jborder");
+            $jborderinfo = $jborder->where('id='.$oid)->find();
+            if ($jborderinfo) {
+                $jborder->where('id='.$oid)->setField('order_status','1');
+                $jborder->where('order_parent='.$oid)->setField('order_status','1');
+            }
+        }
         $this->redirect('Order/lists');
     }
 
     public function sign(){
-        
+        $oid = $this->_get('oid');
+        $order = M("order");
+        $orderinfo = $order->where('id='.$oid)->find();
+        if ($orderinfo) {
+            $order->where('id='.$oid)->setField('order_status','3');
+        } else {
+            $jborder = M("jborder");
+            $jborderinfo = $jborder->where('id='.$oid)->find();
+            if ($jborderinfo) {
+                $jborder->where('id='.$oid)->setField('order_status','3');
+                $jborder->where('order_parent='.$oid)->setField('order_status','3');
+            }
+        }
         $this->redirect('Order/lists');
     }
 
     public function modorder(){
-        
+        $oid = $this->_get('oid');
+        $order = M("order");
+        $orderinfo = $order->where('id='.$oid)->find();
+        if ($orderinfo) {
+            $this->assign('orderinfo', $orderinfo);
+        } else {
+            $jborder = M("jborder");
+            $jborderinfo = $jborder->where('id='.$oid)->find();
+            if ($jborderinfo) {
+                $this->assign('orderinfo', $jborderinfo);
+            }
+        }
         $this->display();
     }
 
+    public function updateorder() {
+        $this->redirect('Order/lists');
+    }
+    
     public function delorder(){
-        
         $this->redirect('Order/lists');
     }
 }
