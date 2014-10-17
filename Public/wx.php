32f6e25d787bd4d13bb1d6afa6c5786d84b57316
zhaobin <zhaobin@deepin.deepin-domain>
2014-10-17 10:36:04 +0800

    add

--- Public/wx.php
+++ Public/wx.php
@@ -52,26 +52,26 @@ class weixin {
         </item>
         ";
         if ($object->EventKey == 'fy_jblx') {
-            $item_str = sprintf($itemTpl, '结伴留学', '结伴留学', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/jblx');
+            $item_str = sprintf($itemTpl, '结伴留学', '找几个小伙伴一起留学走起！结伴留学有更多申请名校的机会', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/jblx');
         } elseif ($object->EventKey == 'fy_jbxx') {
-            $item_str = sprintf($itemTpl, '结伴学习', '结伴学习', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/jbpx');
+            $item_str = sprintf($itemTpl, '结伴学习', '找几个小伙伴一起来结伴学习吧，结伴学习优惠更多噢', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/jbpx');
         } elseif ($object->EventKey == 'fy_tglx') {
-            $item_str = sprintf($itemTpl, '团购留学', '团购留学', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/tglx');
+            $item_str = sprintf($itemTpl, '团购留学', '如果找到3个同学要留学，就有团购留学的机会哦，价格更优惠，申请名校也更多，还在等什么？', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/tglx');
         } elseif ($object->EventKey == 'fy_tgxx') {
-            $item_str = sprintf($itemTpl, '团购学习', '团购学习', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/tgpx');
+            $item_str = sprintf($itemTpl, '团购学习', '寻找5-10个同学一起团购学习吧，不需要跑到很远去学习，人人汇邀请名师来学校给大家讲课', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/tgpx');
         } elseif ($object->EventKey == 'fy_tjpy') {
-            $item_str = sprintf($itemTpl, '推荐朋友', '推荐朋友', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/tjpy');
+            $item_str = sprintf($itemTpl, '推荐朋友', '同学们想挣钱吗？别告诉我你不想，想的话，就把想留学的同学和想学习的同学介绍过来吧', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/tjpy');
         } elseif ($object->EventKey == 'fy_home') {
-            $item_str = sprintf($itemTpl, '微官网', '微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍微官网介绍商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作商务合作手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定手机绑定', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName);
+            $item_str = sprintf($itemTpl, '微官网', '欢迎同学们进入微官网', '', $_SERVER['SERVER_NAME'].'/index.php/wx/'.$object->FromUserName);
         } elseif ($object->EventKey == 'fy_bindphone') {
-            $item_str = sprintf($itemTpl, '手机绑定', '手机绑定', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/regphone');
+            $item_str = sprintf($itemTpl, '手机绑定', '手机绑定后，才可以进行预约咨询', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/regphone');
         } elseif ($object->EventKey == 'fy_swhz') {
             $item_str = sprintf($itemTpl, '商务合作', '商务合作', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/swhz');
         } elseif ($object->EventKey == 'fy_logo') {
             $item_str = sprintf($itemTpl, 'Logo', 'Logo投票', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/logo');
         } elseif ($object->Event == 'subscribe') {
             $item_str = sprintf($itemTpl, '欢迎关注人人汇', '', '', '');
-            $item_str1 = sprintf($itemTpl, '绑定手机', '绑定手机享受更多服务', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/regphone');
+            $item_str1 = sprintf($itemTpl, '绑定手机', '手机绑定后，才可以进行预约咨询', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/regphone');
             $item_str2 = sprintf($itemTpl, 'Logo投票', 'Logo投票', '', $_SERVER['SERVER_NAME'].'/index.php/weixin/'.$object->FromUserName.'/logo');
             $xmlTpl = "<xml>
             <ToUserName><![CDATA[%s]]></ToUserName>
