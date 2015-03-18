a0d6649f5bd0b5718d4edbb59f55c8c63da12da7
mz121star <mz121star@gmail.com>
2015-03-18 14:30:33 +0800

    ok

--- Admin/Lib/Action/IndexAction.class.php
+++ Admin/Lib/Action/IndexAction.class.php
@@ -19,7 +19,52 @@ class IndexAction extends Action {
             $this->redirect('Index/index');
         }
     }
+    public function weixinAction() {
+        if (!$_SESSION['access_token']) {
+            /*  $appid = 'wx746191c3d2d0ebd7';
+              $appsecret = 'a4e835f6b20748fba46a827017cc835a';*/
+             $app_id = 'wxccf0766ad3d06490';
 
+             $app_secret = 'caa282ed24424a7f999f5196b4352ee6';
+            $access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$app_id.'&secret='.$app_secret;
+            $access_token_result = file_get_contents($access_token_url);
+            $access_token_result = json_decode($access_token_result);
+            $_SESSION['access_token'] = $access_token_result->access_token;
+        }
+
+        $underbar_url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token='.$_SESSION['access_token'];
+//            $underbar_content = array('button' => array(array('type'=>'click', 'name'=>'最近吃啥', 'key'=>'ws_zjcs'), array('type'=>'click', 'name'=>'节目汇编', 'key'=>'ws_jmhb'), array('name'=>'精选', 'sub_button'=>array(array('type'=>'click', 'name'=>'精选商品', 'key'=>'ws_jxsp'), array('type'=>'click', 'name'=>'精选活动', 'key'=>'ws_jxhd')))));
+        $underbar_content = '{
+    "button": [
+        {
+            "type": "click",
+            "name": "团购",
+            "key": "fy_tuan"
+        },
+        {
+            "type": "click",
+            "name": "代金券",
+            "key": "fy_tuan"
+        },
+        {
+            "type": "view",
+            "name": "红包",
+            "key": "http://fy.webs.dlwebs.com/index.php/shake"
+        }
+    ]
+}
+                                                       ';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $underbar_url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $underbar_content);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $item_str = curl_exec($ch);
+        curl_close($ch);
+        echo $item_str;exit;
+    }
     public function login(){
         $userInfo = session('userinfo');
         if(!empty($userInfo) && $userInfo['user_type'] != 2){
