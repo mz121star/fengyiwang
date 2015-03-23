9ffe6cc088622eb2abc569a7509ae91098110271
mz121star <mz121star@gmail.com>
2015-03-23 10:53:01 +0800

    ok

--- Admin/Lib/Action/ShakeAction.class.php
+++ Admin/Lib/Action/ShakeAction.class.php
@@ -4,7 +4,7 @@ class ShakeAction extends PublicAction {
 
     public function index(){
 
-            $Model = new \Think\Model(); // 实例化一个model对象 没有对应任何数据表
+        $Model = new Model(); // 实例化一个model对象 没有对应任何数据表
             $result=$Model->query("  SELECT * FROM fy_shake_user s,fy_hongbaorecord h WHERE s.openid=h.openid");
             $this->assign("list",$result);
              $this->display();
